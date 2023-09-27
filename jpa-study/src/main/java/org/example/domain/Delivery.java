4676c33c411ac0afc2cd613d3809617002ddcc9a
daadaadaah <daadaadaah@gmail.com>
2023-09-27 20:33:20 +0900

    [김영한-기본] 예제5 - 연관관계 관리(1) 글로벌 패치 전략 설정
    <글로벌 페치 전략 설정>
    - 즉시 로딩으로 설정하면 사용하지 않는 엔티티도 함께 조회되므로, 모두 `지연 로딩`으로 설정한다.
    - `@OneToOne`, `@ManyToOne`의 fetch 속성을 `지연 로딩`으로 수정한다.
    - `@OneToMany`, `@ManyToMany`는 기본이 지연 로딩이므로, 수정할 필요가 없다.

--- jpa-study/src/main/java/org/example/domain/Delivery.java
+++ jpa-study/src/main/java/org/example/domain/Delivery.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -25,6 +27,6 @@ public class Delivery extends BaseEntity {
 
     private DeliveryStatus status;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 }

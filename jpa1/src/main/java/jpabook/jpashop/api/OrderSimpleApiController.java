87ed10eb0333c2377b4d9e7d4fcad4760640102d
daadaadaah <daadaadaah@gmail.com>
2023-09-30 14:06:18 +0900

    [김영한-활용2] 간단한 주문 조회 V1 : 엔티티를 직접 노출 - 문제 : 무한 루프에 빠짐

---
+++ jpa1/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.api;
+
+import java.util.List;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * xToOne(ManyToOne, OneToOne) 관계 최적화
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ *
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * V1. 엔티티 직접 노출
+     * [문제]
+     * - 무한 루프에 빠짐(https://github.com/daadaadaah/my-jpa/issues/3)
+     *
+     */
+    @GetMapping("/api/simple-orders")
+    public List<Order> orders() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+}

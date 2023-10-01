069efa8c0731cdc12fa3ad40af4c6b7009da746b
daadaadaah <daadaadaah@gmail.com>
2023-10-01 16:22:10 +0900

    [김영한-스프링 데이터 JPA] 쿼리 메소드 기능 1 : 메소드 이름으로 쿼리 생성

--- spring-data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ spring-data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,8 +1,14 @@
 package study.datajpa.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    /**
+     * [스프링 데이터 JPA가 제공하는 쿼리 메소드 기능 1] 메서드 이름으로 쿼리 생성
+     * 네이밍 참고 : https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+     */
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
--- spring-data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ spring-data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package study.datajpa.repository;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +28,20 @@ class MemberRepositoryTest {
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         Assertions.assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+        Assertions.assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        Assertions.assertThat(result.get(0).getAge()).isEqualTo(20);
+        Assertions.assertThat(result.size()).isEqualTo(1);
     }
 }

48b1941090058eda6ff300ff898ea712214a8012
daadaadaah <daadaadaah@gmail.com>
2023-10-05 22:28:36 +0900

    [김영한-QueryDSL] 정렬

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -155,4 +155,28 @@ public class QuerydslBasicTest {
 
         assertThat(count).isEqualTo(4);
     }
+
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+
+        List<Member> result = queryFactory
+            .selectFrom(member)
+            .where(member.age.eq(100))
+            .orderBy(
+                member.age.desc(), // 정렬 1. 회원 나이 내림차순(desc)
+                member.username.asc().nullsLast() // 정룔 2. 회원 이름 올림차순(asc) (단, 회원 이름이 없으면 마지막에 출력(nulls last) )
+            )
+            .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
 }
